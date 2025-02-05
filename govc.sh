94f66dad245a7239847ecb6a2f0bbcaf184ec167
me <me@d1g1tals0ul.com>
2025-02-05 13:20:15 -0700

    initial setup added ova

--- govc.sh
+++ govc.sh
@@ -19,3 +19,9 @@ if [[ $1 == 'pool' ]]
 then
   govc pool.info */Resources
 fi
+
+# https://github.com/kubernetes-sigs/cluster-api-provider-vsphere?tab=readme-ov-file#kubernetes-versions-with-published-ovas
+if [[ $1 == 'ova' ]]
+then
+  govc import.ova -options=spec.json ~/Downloads/ubuntu-2404-kube-v1.32.0.ova
+fi
