90df0a2501fa0e424da6aabfd1d45d71c0c6e6a4
me <me@d1g1tals0ul.com>
2025-02-05 12:14:44 -0700

    initial setup

---
+++ govc.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -eo pipefail
+
+export GOVC_DATACENTER=''
+export GOVC_URL='192.168.1.51'
+export GOVC_HOST='esxi2.cikli.com'
+export GOVC_DATASTORE='datastore1'
+export GOVC_NETWORK='LAN'
+export GOVC_USERNAME='root'
+export GOVC_PASSWORD="$2"
+export GOVC_INSECURE=true
+
+if [[ $1 == 'mark' ]]
+then
+  govc vm.markastemplate "ubuntu-2404-kube-v1.32.0"
+fi
+
+if [[ $1 == 'pool' ]]
+then
+  govc pool.info */Resources
+fi
---
+++ init.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -eo pipefail
+
+command=$1
+cluster_name=$2
+cluster_version=$3
+
+if [ -z "$1" || -z "$2" || -z "$3" ]; then
+  echo "All 3 options must be set: command, cluster name, and cluster version must be set."
+  echo "For example: ./capv.sh init my-cluster 1.30.0"
+  exit 1
+fi
+
+if [[ "$command" == 'init' ]]; then
+  clusterctl init --infrastructure vsphere:v1.12.0 --wait-providers --target-namespace ${cluster_name} --ipam in-cluster --config clusterctl.yaml
+fi
+
+if [[ "$command" == 'create' ]]; then
+  kubectl apply -f sc.yaml
+  kubectl apply -f pvc.yaml
+  kubectl apply -f ipam.yaml
+
+  clusterctl generate cluster ${cluster_name} --infrastructure vsphere:v1.12.0 --flavor node-ipam --kubernetes-version v${cluster_version} --control-plane-machine-count 1 --worker-machine-count 3 > vsphere-cluster-${cluster_name}.yaml
+
+  # This apply will actually create the cluster
+  kubectl apply -f vsphere-cluster-${cluster_name}.yaml
+fi
+
+if [[ "$command" == 'info' ]]; then
+  clusterctl describe cluster ${cluster_name} --echo --grouping=false
+fi
+
+if [[ "$command" == 'delete' ]]; then
+  kubectl delete cluster ${cluster_name}
+fi
