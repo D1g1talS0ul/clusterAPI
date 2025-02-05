7af5890938e37db5141a6d0a394c6dab3c215efe
me <me@d1g1tals0ul.com>
2025-02-05 14:53:02 -0700

    initial setup but not working ATM

--- init.sh
+++ init.sh
@@ -5,22 +5,25 @@ command=$1
 cluster_name=$2
 cluster_version=$3
 
-if [ -z "$1" || -z "$2" || -z "$3" ]; then
+if [[ -z "$1" || -z "$2" || -z "$3" ]]; then
   echo "All 3 options must be set: command, cluster name, and cluster version must be set."
   echo "For example: ./capv.sh init my-cluster 1.30.0"
   exit 1
 fi
 
 if [[ "$command" == 'init' ]]; then
-  clusterctl init --infrastructure vsphere:v1.12.0 --wait-providers --target-namespace ${cluster_name} --ipam in-cluster --config clusterctl.yaml
+  #clusterctl init --infrastructure vsphere:v1.12.0 --wait-providers --target-namespace ${cluster_name} --ipam in-cluster --config clusterctl-ipam.yaml
+  clusterctl init --infrastructure vsphere:v1.12.0 --wait-providers --target-namespace ${cluster_name} --config clusterctl.yaml
 fi
 
-if [[ "$command" == 'create' ]]; then
-  kubectl apply -f sc.yaml
-  kubectl apply -f pvc.yaml
-  kubectl apply -f ipam.yaml
 
-  clusterctl generate cluster ${cluster_name} --infrastructure vsphere:v1.12.0 --flavor node-ipam --kubernetes-version v${cluster_version} --control-plane-machine-count 1 --worker-machine-count 3 > vsphere-cluster-${cluster_name}.yaml
+if [[ "$command" == 'create' ]]; then
+#   kubectl apply -f sc.yaml
+#   kubectl apply -f pvc.yaml
+#   kubectl apply -f ipam.yaml
+
+  #clusterctl generate cluster ${cluster_name} --config clusterctl.yaml --infrastructure vsphere:v1.12.0 --flavor node-ipam --kubernetes-version v${cluster_version} --control-plane-machine-count 1 --worker-machine-count 3 > vsphere-cluster-${cluster_name}.yaml
+  clusterctl generate cluster ${cluster_name} --config clusterctl.yaml --infrastructure vsphere:v1.12.0 --kubernetes-version v${cluster_version} --control-plane-machine-count 1 --worker-machine-count 3 > vsphere-cluster-${cluster_name}.yaml
 
   # This apply will actually create the cluster
   kubectl apply -f vsphere-cluster-${cluster_name}.yaml
